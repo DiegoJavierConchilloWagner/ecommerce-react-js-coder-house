f37a4fee3694d4bf2cdfdf67d0c559c590667647
diegojavierconchillowagner <Diegoo.javier.wagner@gmail.com>
2021-07-21 23:08:32 -0300

    cambio context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -6,16 +6,19 @@ export const useCartContext = () => useContext(CartContext)
 export const CartPrivider = ({children}) => {
     const [cart, setCart] = useState([])
     const [cartNumber, setCartNumber] = useState(0);
-    // const [contador, setContador] = useState(1);
+    const [contador, setContador] = useState(1);
 
     const pushItems = (item) =>{
         setCart([...cart,item])
         setCartNumber(cartNumber + 1) 
     }
+    // const aumentar = () => cantidad < 10 && setContador(contador + 1)como puedo hacer para realizarlo con cantidad? que seria la del stock del objeto
+    const aumentar = () => contador < 10 && setContador(contador + 1) 
+    const disminuir = () => contador > 1 && setContador(contador -1) 
     console.log(cart)
     return (
         <>
-            <CartContext.Provider value={{cart,pushItems,cartNumber}}>
+            <CartContext.Provider value={{cart,pushItems,cartNumber,aumentar,disminuir,contador}}>
                 {children}
             </CartContext.Provider> 
         </>
